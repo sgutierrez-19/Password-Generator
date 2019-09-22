0602c94a656dd4e663ee759a9524582a03f9a129
Steven Gutierrez <stematgut@gmail.com>
2019-09-22 16:01:48 -0700

    added reset css, added arrays for characters, numbers, and cases, and questions to see how user wants password made.

---
+++ index.js
@@ -0,0 +1,20 @@
+// var special = ["!#$%&'()*+,-./:;<=>?@[]^_`{|}~"]
+// var numbers = ["0123456789"]
+// var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+// var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
+
+// var length = prompt("Choose a password length between 8 and 128 characters:")
+//     if (length < 8 || length > 128) {
+//         alert("You must choose a number between 8 and 128.")
+//     } else if (isNaN(length)) {
+//         alert("You must choose a number.")
+//     }
+
+// var useNum = confirm("Would you like your password to contain numbers?")
+// var useSpe = confirm("Would you like your password to contain special characters (e.g. %, @, >, etc?)")
+// var useUpp = confirm("Would you like your password to contain upper case letters?")
+// var useLow = confirm("Would you like your password to contain lower case letters?") 
+
+// if (useNum == false && useSpe == false && useUpp == false && useLow == false) {
+//     alert("Your password must contain one of the preceeding items.  Please try again.")
+// }
