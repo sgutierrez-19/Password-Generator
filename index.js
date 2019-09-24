c662bf672ebc767db9571a951515e5a4681b8b60
Steven Gutierrez <stematgut@gmail.com>
2019-09-23 20:45:24 -0700

    Added copy button and tried to make choices work - added to do list to js

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
-var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
-var numbers = "0123456789".split("");
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var special = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+var numbers = "0123456789";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 
 var choiceArrays = [];
 
 // Asks to choose a length of the password between 8 and 128 characters
 function questions () {
-    var length = prompt("Choose a password length between 8 and 128 characters:")
+    var length = prompt("Choose a password length between 8 and 128 characters:");
         if (length < 8 || length > 128) {
-            alert("You must choose a number between 8 and 128.")
+            alert("You must choose a number between 8 and 128.");
         } else if (isNaN(length)) {
-            alert("You must choose a number.")
+            alert("You must choose a number.");
         }
 
-    var useNum = confirm("Would you like your password to contain numbers?")
+    var useNum = confirm("Would you like your password to contain numbers?");
         if (useNum = true) {
             choiceArrays = choiceArrays + numbers;
         }
 
-    var useSpe = confirm("Would you like your password to contain special characters (e.g. %, @, >, etc?)")
+    var useSpe = confirm("Would you like your password to contain special characters (e.g. %, @, >, etc?)");
     if (useSpe = true) {
         choiceArrays = choiceArrays + special;
     }
 
-    var useUpp = confirm("Would you like your password to contain upper case letters?")
+    var useUpp = confirm("Would you like your password to contain upper case letters?");
     if (useUpp = true) {
         choiceArrays = choiceArrays + upperCase;
     }
 
-    var useLow = confirm("Would you like your password to contain lower case letters?") 
+    var useLow = confirm("Would you like your password to contain lower case letters?");
     if (useLow = true) {
         choiceArrays = choiceArrays + lowerCase;
-    }
+    } 
+    
+    // else if (useNum === false && useSpe === false && useUpp === false && useLow === false) {
+    //     break;
+    // }
 
 
-    document.getElementById("password").innerHTML = genPass (length, choiceArrays)
+    document.getElementById("password").innerHTML = genPass (length, choiceArrays);
 
     }
 
@@ -48,15 +52,14 @@ function questions () {
         return password
     }
 
+function copied() {
+    var copiedText = document.getElementById("password");
+    copiedText.select();
+    document.execCommand("copy");
+}
 
-// Function to pull a random piece out of an array
-// function genPass (len, arr) {
-//     var password = ['']
-//     for (var i = 0; i < len; i++) {
-//         var random = Math.floor(Math.random() * arr.length)
-//         password = password + arr[random];
-//     } 
-//     return password
-// }
-
-// console.log(lowerCase)
+// 1) need to stop func if proper length isn't selected
+// 2) need to stop func if no options selected
+    // tried break; and return; but break makes whole function not work and return just keeps going
+// 3) choiceArrays isn't compiling properly
+// 4) Add styling, positioning, and responsiveness 
